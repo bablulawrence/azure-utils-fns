b7bf8f4cc3c740a5c0180ec8c7e779171dd11a4b
Bablu Lawrence <v-balawrence@example.com>
2022-06-14 15:26:16 +0530

    Added logic for passing the subcription id

--- fn-drbl-http-starter/__init__.py
+++ fn-drbl-http-starter/__init__.py
@@ -1,20 +1,15 @@
-# This function an HTTP starter function for Durable Functions.
-# Before running this sample, please:
-# - create a Durable orchestration function
-# - create a Durable activity function (default name is "Hello")
-# - add azure-functions-durable to requirements.txt
-# - run pip install -r requirements.txt
- 
 import logging
-
 import azure.functions as func
 import azure.durable_functions as df
 
-
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     client = df.DurableOrchestrationClient(starter)
-    instance_id = await client.start_new(req.route_params["functionName"], None, None)
 
+    #Get orchastration function name and data
+    function_name = req.route_params["functionName"]
+    data = req.get_json()
+    
+    #Call orchastrator function
+    instance_id = await client.start_new(function_name, client_input=data)
     logging.info(f"Started orchestration with ID = '{instance_id}'.")
-
     return client.create_check_status_response(req, instance_id)
--- fn-drbl-pause-sql-dp-orchstr/__init__.py
+++ fn-drbl-pause-sql-dp-orchstr/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from collections import namedtuple
 from urllib import response
 import azure.functions as func
@@ -20,21 +21,26 @@ def get_sqldp_params_list(subscription_id, sqldp_list):
     
 def orchestrator_function(context: df.DurableOrchestrationContext):
     logging.info("Starting execution of orchastrator function")
-    
-    subscription_id = 'edf6dd9d-7c4a-4bca-a997-945f3d60cf4e'
-    
-    #Get the list of dedicated pools in the subscription
-    query= 'project id, name, type, properties.status, resourceGroup | where type =~ "microsoft.synapse/workspaces/sqlpools"'
-    res_query_params = ResQueryParams(subscriptionId=subscription_id, query=query)
-    query_result = yield context.call_activity('fn-drbl-query-resource-graph-activity', res_query_params)
-    sqldp_list = query_result['data']
 
-    #Pause the dedicated pools
-    sqldp_params_list = get_sqldp_params_list(subscription_id, sqldp_list)
-    sqldp_result_tasks = [ context.call_activity('fn-drbl-pause-sql-dp-activity', sqldp_params) for sqldp_params in sqldp_params_list]
+    #Get subscription id from input
+    params = context.get_input()
+    subscription_id = params['subscriptionId']
     
-    #Combine and return the results
-    sqldp_result = yield context.task_all(sqldp_result_tasks)
-    return sqldp_result
+    if (subscription_id):
+        #Get the list of dedicated pools in the subscription
+        query= 'project id, name, type, properties.status, resourceGroup | where type =~ "microsoft.synapse/workspaces/sqlpools"'
+        res_query_params = ResQueryParams(subscriptionId=subscription_id, query=query)
+        query_result = yield context.call_activity('fn-drbl-query-resource-graph-activity', res_query_params)
+        sqldp_list = query_result['data']
+
+        #Pause the dedicated pools
+        sqldp_params_list = get_sqldp_params_list(subscription_id, sqldp_list)
+        sqldp_result_tasks = [ context.call_activity('fn-drbl-pause-sql-dp-activity', sqldp_params) for sqldp_params in sqldp_params_list]
+        
+        #Combine and return the results
+        sqldp_result = yield context.task_all(sqldp_result_tasks)
+        return sqldp_result
+    else:
+        return "Please provide subscription id"
 
 main = df.Orchestrator.create(orchestrator_function)
